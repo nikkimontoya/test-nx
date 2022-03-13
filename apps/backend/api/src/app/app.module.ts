59ed1a14900fbdd2d31238f0c304fb4894bbf343
sviridov <sviridov.dev@gmail.com>
2022-03-13 18:19:58 +0300

    typeorm

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { environment } from '../environments/environment';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TypeOrmModule.forRoot({
+        ...environment.connection
+    })],
+    controllers: [AppController],
+    providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/backend/api/src/environments/environment.ts
+++ apps/backend/api/src/environments/environment.ts
@@ -1,3 +1,23 @@
 export const environment = {
   production: false,
+  connection: {
+    type: process.env.DB_TYPE as 'aurora-data-api',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_USER_PASSWORD,
+    database: process.env.DB_NAME,
+    dropSchema: false,
+    synchronize: true,
+    logging: true,
+    entities: [
+      'dist/apps/backend/api/src/**/*.entity{.ts, .js}'
+    ],
+    migrations: [
+      'dist/apps/backend/api/migrations/*{.ts, .js}'
+    ],
+    cli: {
+      'migrationsDir': 'db/migrations'
+    }
+  }
 };
