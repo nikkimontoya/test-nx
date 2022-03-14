65ab656efffcb4755b9a52fbee3e98221e941985
sviridov <sviridov.dev@gmail.com>
2022-03-14 18:36:33 +0300

    graphql

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { environment } from '../environments/environment';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
-        ...environment.connection
-    })],
+    imports: [
+        TypeOrmModule.forRoot({
+            ...environment.connection
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            typePaths: ['./**/*.graphql'],
+            context: ({ req }) => ({ req }),
+            playground: true,
+            driver: ApolloDriver
+        })
+    ],
     controllers: [AppController],
-    providers: [AppService]
+    providers: [AppResolver]
 })
 export class AppModule {
 }
