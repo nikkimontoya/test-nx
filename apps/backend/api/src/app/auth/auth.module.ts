ca80630255d90aa0382e0f3b133070f63fc4dd97
sviridov <sviridov.dev@gmail.com>
2022-04-02 20:33:58 +0300

    auth module

--- apps/backend/api/src/app/auth/auth.module.ts
+++ apps/backend/api/src/app/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthResolver } from './resolvers/auth.resolver';
+import {UserService} from '../user/services/user.service';
+import {PassportModule} from '@nestjs/passport';
+import {JwtModule} from '@nestjs/jwt';
+import {environment} from '../../environments/environment';
+import {PasswordService} from './services/password.service';
+import {JwtStrategy} from './services/jwt.strategy';
 
 @Module({
-    providers: [AuthService, AuthResolver]
+    imports: [
+        UserService,
+        PassportModule.register({
+            defaultStrategy: 'jwt'
+        }),
+        JwtModule.register({
+            privateKey: environment.jwt.secret,
+            signOptions: {
+                expiresIn: environment.jwt.expiresIn
+            }
+        })
+    ],
+    exports: [AuthService, PassportModule],
+    providers: [AuthService, AuthResolver, PasswordService, JwtStrategy]
 })
 export class AuthModule {}
