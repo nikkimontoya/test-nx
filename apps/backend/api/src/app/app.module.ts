15336d83f3017838706e387e65683688be50df22
sviridov <sviridov.dev@gmail.com>
2022-04-02 20:42:51 +0300

    Graphql resolver

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {GraphQLModule} from '@nestjs/graphql';
+import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
 
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { environment } from '../environments/environment';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AppResolver } from './app.resolver';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import {AppController} from './app.controller';
+import {environment} from '../environments/environment';
+import {UserModule} from './user/user.module';
+import {AuthModule} from './auth/auth.module';
+import {resolverMap} from './app.resolver';
+import {UserEntity} from './user/entities/user.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      ...environment.connection,
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      typePaths: ['./**/*.graphql'],
-      context: ({ req }) => ({ req }),
-      playground: true,
-      driver: ApolloDriver,
-    }),
-    UserModule,
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppResolver],
+    imports: [
+        TypeOrmModule.forRoot({
+            ...environment.connection,
+            entities: [UserEntity]
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            typePaths: ['./**/*.graphql'],
+            context: ({req}) => ({req}),
+            playground: true,
+            driver: ApolloDriver,
+            resolvers: [resolverMap]
+        }),
+        UserModule,
+        AuthModule,
+    ],
+    controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/backend/api/src/app/app.resolver.ts
+++ apps/backend/api/src/app/app.resolver.ts
@@ -1,5 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import GraphQLJSON from 'graphql-type-json';
+import {GraphQLScalarType} from 'graphql';
+import {Kind} from 'graphql/language';
 
-@Resolver('app')
-export class AppResolver {
+export const resolverMap = {
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+        serialize(value: Date) {
+            return value.getTime(); // Convert outgoing Date to integer for JSON
+        },
+        parseValue(value: number) {
+            return new Date(value); // Convert incoming integer to Date
+        },
+        parseLiteral(ast) {
+            if (ast.kind === Kind.INT) {
+                return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+            }
+            return null; // Invalid hard-coded value (not an integer)
+        },
+    }),
+    JSON: GraphQLJSON
 }
